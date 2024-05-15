4839df27e293e32c960c39e1329d28c8e697eb23
marquestye <e0360581@u.nus.edu>
2024-05-15 16:18:19 +0800

    Fix scripts

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -6,7 +6,3 @@ cd /home/ec2-user/Overview/server >> /home/ec2-user/Overview/server/deploy.log
 
 echo 'npm install' >> /home/ec2-user/Overview/server/deploy.log 
 npm install >> /home/ec2-user/Overview/server/deploy.log
-
-echo 'npm run build' >> /home/ec2-user/Overview/server/deploy.log
-npm run build >> /home/ec2-user/Overview/server/deploy.log
-
