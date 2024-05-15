eabfd9fe597fd8726f4ae18c5c4436d7c5d37091
Aung <74162870+15WattPowerBrick@users.noreply.github.com>
2024-05-15 14:31:24 +0800

    Update after_install.sh

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/Overview/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/Overview/server/deploy.log
 
-echo 'cd /home/ec2-user/Overview' >> /home/ec2-user/Overview/deploy.log
-cd /home/ec2-user/Overview >> /home/ec2-user/Overview/deploy.log
+echo 'cd /home/ec2-user/Overview/server' >> /home/ec2-user/Overview/server/deploy.log
+cd /home/ec2-user/Overview/server >> /home/ec2-user/Overview/server/deploy.log
 
-echo 'npm install' >> /home/ec2-user/Overview/deploy.log 
-npm install >> /home/ec2-user/Overview/deploy.log
+echo 'npm install' >> /home/ec2-user/Overview/server/deploy.log 
+npm install >> /home/ec2-user/Overview/server/deploy.log
 
