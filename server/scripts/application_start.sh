93a72a30dddee0af1c8197e3286a47bb08ff357b
marquestye <e0360581@u.nus.edu>
2024-05-15 16:08:32 +0800

    Fix server scripts

--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/Overview/server/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'pm2 restart nodejs-app' >> /home/ec2-user/Overview/server/deploy.log
-pm2 restart server >> /home/ec2-user/Overview/server/deploy.log
+echo 'pm2 restart Overview' >> /home/ec2-user/Overview/server/deploy.log
+pm2 restart Overview >> /home/ec2-user/Overview/server/deploy.log
