5a83fbdcd37d4b6a86e3e48e80b112bc0223c240
15WattPowerBrick <74162870+15WattPowerBrick@users.noreply.github.com>
2024-05-20 14:57:19 +0800

    add private route, dropdown to dashboard, getexisting endpoint

--- server/src/routes/api/admin/project.ts
+++ server/src/routes/api/admin/project.ts
@@ -33,6 +33,23 @@ projectAPIRouter.get("/project/:projectId", async (req, res) => {
   return res.send(projectData);
 });
 
+projectAPIRouter.get("/getExisting", async (req, res) => {
+  try {
+    const projectData = await prisma.project.findMany({
+      select: {
+        oldReferenceId: true,
+      },
+    }) as { oldReferenceId: string | null }[];
+
+    // Filter out null values if you only want non-null oldReferenceIds
+    const oldReferenceIds = projectData.map(project => project.oldReferenceId).filter(id => id !== null);
+
+    res.json(oldReferenceIds);
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+})
+
 projectAPIRouter.get("/projects", async (req, res) => {
   const user = req.user as User;
 
