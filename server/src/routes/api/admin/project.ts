3230aed3b80192b9ee9eab08b53033d7c57724a2
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-05-21 14:37:06 +0800

    Remove /project API route

--- server/src/routes/api/admin/project.ts
+++ server/src/routes/api/admin/project.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+import { MCICompany, PrismaClient, Role } from "@prisma/client";
 import { PrismaError, User } from "@/types";
 import checkPermission from "../../../utils/check-permission";
 
 const prisma = new PrismaClient();
 
 const projectAPIRouter: Router = Router();
-
-projectAPIRouter.get("/project", async (_req, res) => {
-  return res.status(400).send("No project ID specified.");
-});
 
 projectAPIRouter.get("/project/:projectId", async (req, res) => {
   const user = req.user as User;
@@ -42,25 +38,8 @@ projectAPIRouter.get("/project/:projectId", async (req, res) => {
     return res.send(projectData);
   }
 
-  return res.status(401).send("Unauthorized");
+  return res.status(401).send('Unauthorized. User does not have permission "canReadAllProjects".');
 });
-
-projectAPIRouter.get("/getExisting", async (req, res) => {
-  try {
-    const projectData = await prisma.project.findMany({
-      select: {
-        oldReferenceId: true,
-      },
-    }) as { oldReferenceId: string | null }[];
-
-    // Filter out null values if you only want non-null oldReferenceIds
-    const oldReferenceIds = projectData.map(project => project.oldReferenceId).filter(id => id !== null);
-
-    res.json(oldReferenceIds);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-})
 
 projectAPIRouter.get("/projects", async (req, res) => {
   const user = req.user as User;
@@ -83,7 +62,7 @@ projectAPIRouter.get("/projects/all", async (req, res) => {
   const hasPermission = await checkPermission(user.id, "canReadAllProjects");
 
   if (!hasPermission) {
-    return res.status(401).send("Unauthorized");
+    return res.status(401).send('Unauthorized. User does not have permission "canReadAllProjects".');
   }
 
   const projectsData = await prisma.project.findMany();
@@ -98,53 +77,63 @@ projectAPIRouter.post("/project/create", async (req, res) => {
   const name = req.body.name;
   const clientId = req.body.clientId;
 
-  let employmentBy = req.body.employmentBy;
-  let locations = req.body.locations;
-  let startDate = req.body.startDate;
-  let endDate = req.body.endDate;
+  const employmentBy = req.body.employmentBy;
+  const locations = req.body.locations;
+  const startDate = req.body.startDate;
+  const endDate = req.body.endDate;
 
   // Optional fields
-  const status = req.body.status || "ACTIVE";
+  const status = req.body.status;
   const candidateHolders = req.body.candidateHolders || [];
 
-  if (!name || !clientId || !locations || !startDate || !endDate) {
+  if (!name || !clientId || !employmentBy || !locations || !startDate || !endDate) {
     return res
       .status(400)
-      .send("name, clientId, locations, startDate, and endDate are required.");
+      .send("name, clientId, locations, startDate, and endDate parameters are required.");
   }
 
-  if (
-    employmentBy !== "MCI Career Services Pte Ltd" &&
-    employmentBy !== "MCI Outsourcing Pte Ltd"
-  ) {
-    return res
-      .status(400)
-      .send(
-        'Invalid employmentBy value. Must be either "MCI Career Services" or "MCI Outsourcing Pte Ltd"',
-      );
+  // Validation of employmentBy
+  switch (employmentBy) {
+    case "MCI Career Services Pte Ltd":
+      employmentBy = MCICompany.MCI_CAREER_SERVICES;
+      break;
+    case "MCI Outsourcing Pte Ltd":
+      employmentBy = MCICompany.MCI_OUTSOURCING;
+      break;
+    default:
+      return res
+        .status(400)
+        .send(
+          'Invalid employmentBy value. Must be either "MCI Career Services Pte Ltd" or "MCI Outsourcing Pte Ltd"',
+        );
   }
 
+  // Validation of candidateHolders
   if (!Array.isArray(candidateHolders)) {
     return res.status(400).send("candidateHolders must be an array.");
   }
 
+  // Validation of locations
   try {
     locations = JSON.parse(locations);
   } catch (e) {
     return res.status(400).send("locations must be a valid JSON string.");
   }
 
+  // Validation of endDate
+  try {
+    endDate = new Date(endDate);
+  } catch (e) {
+    return res.status(400).send("endDate must be a valid date.");
+  }
+
+  // Validation of startDate
   try {
     startDate = new Date(startDate);
   } catch (e) {
     return res.status(400).send("startDate must be a valid date.");
   }
 
-  try {
-    endDate = new Date(endDate);
-  } catch (e) {
-    return res.status(400).send("endDate must be a valid date.");
-  }
 
   if (employmentBy === "MCI Career Services Pte Ltd") {
     employmentBy = "MCI_CAREER_SERVICES";
@@ -171,12 +160,23 @@ projectAPIRouter.post("/project/create", async (req, res) => {
               ...candidateHolders.map((email: string) => {
                 return {
                   consultantEmail: email,
-                  Role: "CANDIDATE_HOLDER",
+                  role: Role.CANDIDATE_HOLDER,
                 };
               }),
             ],
           },
         },
+        Client: {
+          connectOrCreate: {
+            where: {
+              id: clientId,
+            },
+            create: {
+              id: clientId,
+            },
+          },
+          }
+        }
       },
     });
   } catch (e) {
@@ -231,7 +231,19 @@ projectAPIRouter.post("/project/update", async (req, res) => {
   const user = req.user as User;
   const projectId = req.body.projectId;
 
-  // TODO: Implement project update logic
+  const clientHolderEmails: string[] = req.body.clientHolderEmails;
+  const candidateHolderEmails: string[] = req.body.candidateHolderEmails;
+  const candidateNRICs: string[] = req.body.candidateNRICs;
+
+  await prisma.$transaction([
+    prisma.manage.deleteMany({
+      where: {
+        projectId: projectId,
+      },
+    }),
+
+
+
 
   try {
     await prisma.project.update({
@@ -251,5 +263,64 @@ projectAPIRouter.post("/project/update", async (req, res) => {
 
   return res.send("Project updated");
 });
+
+projectAPIRouter.post("/project/assign", async (req, res) => {
+  const user = req.user as User;
+  const projectId = req.body.projectId;
+
+  const consultantEmails: string[] = req.body.consultantEmails;
+  const candidateNRICs:string[] = req.body.candidateNRICs;
+
+  if (!projectId || !consultantEmails || !candidateNRICs) {
+    return res.status(400).send("projectId, consultantEmail and candidateNRICs are required.");
+  }
+
+  try {
+    await prisma.project.update({
+      where: {
+        id: projectId,
+        Manage: {
+          some: {
+            consultantEmail: user.id,
+          },
+        },
+      },
+      data: {
+        Manage: {
+          createMany: {
+            data: [
+              ...consultantEmails.map((email: string) => {
+                return {
+                  consultantEmail: email,
+                  Role: Role.,
+                };
+              }),
+              ...candidateNRICs.map((nric: string) => {
+                return {
+                  consultantEmail: nric,
+                  Role: "CANDIDATE",
+                };
+              }),
+            ],
+          },
+        },
+      },
+    });
+  } catch (e) {
+    const error = e as PrismaError;
+
+    if (error.code === "P2025") {
+      return res.status(404).send("Project does not exist.");
+    }
+
+    if (error.code === "P2016") {
+      return res.status(400).send("Consultant does not exist.");
+    }
+
+    return res.status(500).send("Internal server error.");
+  }
+
+  return res.send("Consultant assigned to project");
+}
 
 export default projectAPIRouter;
