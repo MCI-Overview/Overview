a7911897d6d06f2950d82bac316d3dd7e6ce8ab0
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-05-21 08:46:58 +0800

    Fix consultant routes not added

--- server/src/routes/api/admin/index.ts
+++ server/src/routes/api/admin/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import clientAPIRoutes from "./client";
 import projectAPIRoutes from "./project";
 import consultantAPIRoutes from "./consultant";
+import candidateAPIRoutes from "./candidate";
 
 const adminAPIRouter: Router = Router();
 
 adminAPIRouter.use("/", clientAPIRoutes);
 adminAPIRouter.use("/", projectAPIRoutes);
 adminAPIRouter.use("/", consultantAPIRoutes);
+adminAPIRouter.use("/", candidateAPIRoutes);
 
 export default adminAPIRouter;
