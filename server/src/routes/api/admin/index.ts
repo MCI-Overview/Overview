f681905e0a6da6029fb31831e3fa67af7f2ecdf6
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-05-23 09:57:38 +0800

    Make shift API routes accessible

--- server/src/routes/api/admin/index.ts
+++ server/src/routes/api/admin/index.ts
@@ -1,14 +1,16 @@
 import { Router } from "express";
+import shiftAPIRoutes from "./shift";
 import clientAPIRoutes from "./client";
 import projectAPIRoutes from "./project";
-import consultantAPIRoutes from "./consultant";
 import candidateAPIRoutes from "./candidate";
+import consultantAPIRoutes from "./consultant";
 
 const adminAPIRouter: Router = Router();
 
+adminAPIRouter.use("/", shiftAPIRoutes);
 adminAPIRouter.use("/", clientAPIRoutes);
 adminAPIRouter.use("/", projectAPIRoutes);
-adminAPIRouter.use("/", consultantAPIRoutes);
 adminAPIRouter.use("/", candidateAPIRoutes);
+adminAPIRouter.use("/", consultantAPIRoutes);
 
 export default adminAPIRouter;
