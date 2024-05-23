1e8ed05dbe0c7343a54c74bfca4b146c9b96e302
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-05-23 12:03:43 +0800

    Add get roster API endpoint

--- server/src/routes/api/admin/roster.ts
+++ server/src/routes/api/admin/roster.ts
@@ -11,6 +11,8 @@ import moment from "moment";
 const prisma = new PrismaClient();
 const rosterAPIRouter: Router = Router();
 
+const FOURTEEN_DAYS_OFFSET = 14 * 24 * 60 * 60 * 1000;
+
 function enumerateDays(startDate: Date, endDate: Date) {
   const start = moment(startDate);
   const end = moment(endDate);
@@ -22,6 +24,211 @@ function enumerateDays(startDate: Date, endDate: Date) {
 
   return dates;
 }
+
+rosterAPIRouter.get(
+  "/roster/:candidateNRIC&:projectId&:startDate&:endDate",
+  async (req: Request, res: Response) => {
+    const user = req.user as User;
+    const { candidateNRIC, projectId, startDate, endDate } = req.params;
+
+    if (!candidateNRIC || !projectId) {
+      return res.status(400).send("candidateNRIC or projectId is required.");
+    }
+
+    let startDateObject = new Date();
+    startDateObject.setTime(startDateObject.getTime() - FOURTEEN_DAYS_OFFSET);
+
+    if (startDate) {
+      try {
+        startDateObject = new Date(startDate);
+      } catch (error) {
+        return res.status(400).send("Invalid start date.");
+      }
+    }
+
+    let endDateObject = new Date();
+    endDateObject.setTime(endDateObject.getTime() + FOURTEEN_DAYS_OFFSET);
+
+    if (endDate) {
+      try {
+        endDateObject = new Date(endDate);
+      } catch (error) {
+        return res.status(400).send("Invalid end date.");
+      }
+    }
+
+    if (startDateObject > endDateObject) {
+      return res.status(400).send("Start date cannot be later than end date.");
+    }
+
+    if (candidateNRIC && projectId) {
+      const attendanceData = await prisma.attendance.findMany({
+        where: {
+          candidateNric: candidateNRIC,
+          status: null,
+          shiftDate: {
+            gte: startDateObject,
+            lte: endDateObject,
+          },
+          Shift: {
+            projectId: projectId,
+          },
+        },
+        include: {
+          Shift: {
+            include: {
+              Project: {
+                include: {
+                  Manage: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      if (
+        attendanceData.some((shift) =>
+          shift.Shift.Project.Manage.some(
+            (consultant) => consultant.consultantEmail === user.id,
+          ),
+        )
+      ) {
+        return res.send(attendanceData);
+      }
+
+      const hasReadAllProjectPermission = await checkPermission(
+        user.id,
+        Permission.CAN_READ_ALL_PROJECTS,
+      );
+
+      if (hasReadAllProjectPermission) {
+        return res.send(attendanceData);
+      }
+
+      return res
+        .status(403)
+        .send(PermissionErrorMessage.CANNOT_READ_PROJECT_ERROR_MESSAGE);
+    }
+
+    if (candidateNRIC && !projectId) {
+      const hasReadAllProjectPermission = await checkPermission(
+        user.id,
+        Permission.CAN_READ_ALL_PROJECTS,
+      );
+
+      if (hasReadAllProjectPermission) {
+        const attendanceData = await prisma.attendance.findMany({
+          where: {
+            candidateNric: candidateNRIC,
+            status: null,
+            shiftDate: {
+              gte: startDateObject,
+              lte: endDateObject,
+            },
+          },
+          include: {
+            Shift: {
+              include: {
+                Project: {
+                  include: {
+                    Manage: true,
+                  },
+                },
+              },
+            },
+          },
+        });
+
+        return res.send(attendanceData);
+      }
+
+      const attendanceData = await prisma.attendance.findMany({
+        where: {
+          candidateNric: candidateNRIC,
+          status: null,
+          shiftDate: {
+            gte: startDateObject,
+            lte: endDateObject,
+          },
+          Shift: {
+            Project: {
+              Manage: {
+                some: {
+                  consultantEmail: user.id,
+                },
+              },
+            },
+          },
+        },
+        include: {
+          Shift: {
+            include: {
+              Project: {
+                include: {
+                  Manage: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      return res.send(attendanceData);
+    }
+
+    if (projectId && !candidateNRIC) {
+      const attendanceData = await prisma.attendance.findMany({
+        where: {
+          status: null,
+          shiftDate: {
+            gte: startDateObject,
+            lte: endDateObject,
+          },
+          Shift: {
+            projectId: projectId,
+          },
+        },
+        include: {
+          Shift: {
+            include: {
+              Project: {
+                include: {
+                  Manage: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+      if (
+        attendanceData.some((shift) =>
+          shift.Shift.Project.Manage.some(
+            (consultant) => consultant.consultantEmail === user.id,
+          ),
+        )
+      ) {
+        return res.send(attendanceData);
+      }
+
+      const hasReadAllProjectPermission = await checkPermission(
+        user.id,
+        Permission.CAN_READ_ALL_PROJECTS,
+      );
+
+      if (hasReadAllProjectPermission) {
+        return res.send(attendanceData);
+      }
+
+      return res
+        .status(403)
+        .send(PermissionErrorMessage.CANNOT_READ_PROJECT_ERROR_MESSAGE);
+    }
+
+    return res.status(400).send("Invalid request.");
+  },
+);
 
 rosterAPIRouter.post("/roster", async (req: Request, res: Response) => {
   const user = req.user as User;
