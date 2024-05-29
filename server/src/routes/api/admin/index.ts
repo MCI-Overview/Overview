c575626859c104f9cd4d3e1cb0a681fc452293ad
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-05-29 17:25:29 +0800

    Delete roster API routes

--- server/src/routes/api/admin/index.ts
+++ server/src/routes/api/admin/index.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
 import shiftAPIRoutes from "./shift";
 import clientAPIRoutes from "./client";
-import rosterAPIRoutes from "./roster";
 import projectAPIRoutes from "./project";
 import candidateAPIRoutes from "./candidate";
 import consultantAPIRoutes from "./consultant";
-import attendanceAPIRoutes from "./attendance";
 
 const adminAPIRouter: Router = Router();
 
@@ -14,7 +12,5 @@ adminAPIRouter.use("/", clientAPIRoutes);
 adminAPIRouter.use("/", projectAPIRoutes);
 adminAPIRouter.use("/", candidateAPIRoutes);
 adminAPIRouter.use("/", consultantAPIRoutes);
-adminAPIRouter.use("/", rosterAPIRoutes);
-adminAPIRouter.use("/", attendanceAPIRoutes);
 
 export default adminAPIRouter;
