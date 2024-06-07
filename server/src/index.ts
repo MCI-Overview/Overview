5241082f842b54a93bf3e82e9b3f2b046b787489
15WattPowerBrick <74162870+15WattPowerBrick@users.noreply.github.com>
2024-06-07 11:15:48 +0800

    revert access-control

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,6 @@ import routes from "./routes";
 import passport from "passport";
 import session from "express-session";
 import PgSession from "connect-pg-simple";
-import { Request, Response, NextFunction } from 'express';
 
 dotenv.config();
 
@@ -15,14 +14,6 @@ const pgSession = PgSession(session);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// this is for testing cookies
-app.use((req: Request, res: Response, next: NextFunction) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,UPDATE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-  next();
-});
 
 app.use(
   session({
