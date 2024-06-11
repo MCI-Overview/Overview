c51c7c405e3027ab003a3710103b6b1c2e2c953f
15WattPowerBrick <74162870+15WattPowerBrick@users.noreply.github.com>
2024-06-11 10:21:07 +0800

    revert changes to env server_url

--- attendance-module/src/App.tsx
+++ attendance-module/src/App.tsx
@@ -27,10 +27,7 @@ import { ProjectContextProvider } from "./providers/projectContextProvider";
 import axiosRetry from "axios-retry";
 import CandidateProfile from "./user/Profile";
 
-const SERVER_URL =
-  import.meta.env.NODE_ENV === "production"
-    ? import.meta.env.VITE_SERVER_URL
-    : "http://localhost:3000";
+const SERVER_URL = import.meta.env.VITE_SERVER_URL;
 
 function App() {
   const location = useLocation();
