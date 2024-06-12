17ad3670fbc39b0b31b98dcfd43c2d7156aed6dc
15WattPowerBrick <74162870+15WattPowerBrick@users.noreply.github.com>
2024-06-12 09:18:00 +0800

    add new user input

--- attendance-module/src/user/UserNewStepper.tsx
+++ attendance-module/src/user/UserNewStepper.tsx
@@ -1,57 +1,131 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
+import { useState, useEffect, ChangeEvent } from "react";
 import {
   Button,
   Stepper,
   Step,
   StepIndicator,
+  stepIndicatorClasses,
+  stepClasses,
   Divider,
   Stack,
   Typography,
   FormControl,
   FormLabel,
   Input,
-  stepClasses,
-  stepIndicatorClasses,
+  Autocomplete,
+  Grid,
+  Checkbox,
 } from "@mui/joy";
 import ContactsRoundedIcon from "@mui/icons-material/ContactsRounded";
 import CheckCircleRoundedIcon from "@mui/icons-material/CheckCircleRounded";
 import WavingHandIcon from "@mui/icons-material/WavingHand";
 import { useUserContext } from "../providers/userContextProvider";
-import { CandidateUser } from "../types/common";
+import { CandidateUser, User } from "../types/common";
+import axios from "axios";
 
 export default function UserNewStepper() {
   const userData = useUserContext().user as CandidateUser | null;
   if (!userData) return null;
 
-  console.log(userData.dateOfBirth);
-
-  const navigate = useNavigate();
   const [step, setStep] = useState(0);
+  const [formData, setFormData] = useState({
+    postalCode: "",
+    addressDetails: {
+      block: "",
+      street: "",
+      building: "",
+    },
+    isPostalCodeValid: true,
+    isLandedProperty: false,
+    nationality: "",
+    floor: "",
+    unit: "",
+  });
+  const [isFormValid, setIsFormValid] = useState(false);
+  const { setUser } = useUserContext();
 
-  // useEffect(() => {
-  //   // retrieve and set user data
-  // }, []);
+  useEffect(() => {
+    const { postalCode, isPostalCodeValid, isLandedProperty, floor, unit, nationality } = formData;
+    const isValid = nationality !== "" && postalCode.length === 6 && isPostalCodeValid &&
+      (isLandedProperty || (floor !== "" && unit !== ""));
+    setIsFormValid(isValid);
+  }, [formData]);
 
-  const handleNext = () => {
-    setStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
+  const handleNext = () => setStep((prevStep) => Math.min(prevStep + 1, steps.length - 1));
+  const handleBack = () => setStep((prevStep) => Math.max(prevStep - 1, 0));
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
-  const handleBack = () => {
-    setStep((prevStep) => Math.max(prevStep - 1, 0));
+  const handlePostalCodeChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const code = event.target.value;
+    setFormData((prevData) => ({
+      ...prevData,
+      postalCode: code,
+      isPostalCodeValid: false,
+      addressDetails: {
+        block: "",
+        street: "",
+        building: "",
+      },
+    }));
+
+    if (code.length === 6) {
+      try {
+        const response = await fetch(
+          `https://www.onemap.gov.sg/api/common/elastic/search?searchVal=${code}&returnGeom=N&getAddrDetails=Y`
+        );
+        const data = await response.json();
+        if (data.found > 0) {
+          const { BLK_NO, ROAD_NAME, BUILDING } = data.results[0];
+          setFormData((prevData) => ({
+            ...prevData,
+            isPostalCodeValid: true,
+            addressDetails: {
+              block: BLK_NO,
+              street: ROAD_NAME,
+              building: BUILDING,
+            },
+          }));
+        }
+      } catch (error) {
+        console.error("Error fetching address details:", error);
+      }
+    }
   };
 
-  const handleSubmit = () => {
-    // Submit user data to backend
-    console.log(userData);
-    // Move to next step or show completion message
-    handleNext();
-  };
+  const handleSubmit = () => handleNext();
 
-  const handleFinish = () => {
-    navigate("/user/home");
-  };
+  const handleFinish = async () => {
+    const { nationality, addressDetails, postalCode, floor, unit } = formData;
+    try {
+      const response = await axios.post(
+        `/api/user/${userData.cuid}/newuser`,
+        {
+          nationality: nationality,
+          address: {
+            unit: unit,
+            floor: floor,
+            building: addressDetails.building,
+            block: addressDetails.block,
+            street: addressDetails.street,
+            postal: postalCode,
+            country: "SINGAPORE",
+          }
+        }
+      );
+      setUser(response.data as User);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  }
+
+  const nationalities = ["Singapore", "Malaysia", "China"];
 
   const steps = [
     {
@@ -60,8 +134,7 @@ export default function UserNewStepper() {
       content: (
         <>
           <Typography level="body-sm">
-            We’re glad you’re here. Tell us a bit about yourself so we can track
-            your attendance accurately. Ready to begin?
+            We’re glad you’re here. Tell us a bit about yourself so we can track your attendance accurately. Ready to begin?
           </Typography>
           <Button onClick={handleNext}>Next</Button>
         </>
@@ -72,50 +145,105 @@ export default function UserNewStepper() {
       icon: <ContactsRoundedIcon />,
       content: (
         <>
+          <Typography level="title-lg">Personal particulars</Typography>
+          <Divider />
           <FormControl required>
             <FormLabel>NRIC</FormLabel>
-            <Input
-              name="nric"
-              value={userData.nric}
-            />
+            <Input name="nric" value={userData.nric} disabled />
           </FormControl>
           <FormControl required>
             <FormLabel>Full Name</FormLabel>
-            <Input
-              name="name"
-              value={userData.name}
-            />
+            <Input name="name" value={userData.name} disabled />
           </FormControl>
           <FormControl required>
             <FormLabel>Date of Birth</FormLabel>
-            <Input
-              type="date"
-              name="dateOfBirth"
-            />
+            <Input type="date" name="dateOfBirth" disabled />
           </FormControl>
           <FormControl required>
             <FormLabel>Contact Number</FormLabel>
-            <Input
-              name="contact"
-              value={userData.contact}
-            />
+            <Input name="contact" value={userData.contact} disabled />
           </FormControl>
           <FormControl required>
             <FormLabel>Nationality</FormLabel>
-            <Input
+            <Autocomplete
               name="nationality"
+              options={nationalities}
+              value={formData.nationality ? nationalities.find(option => option === formData.nationality) : null}
+              onChange={(_event, newValue) => setFormData({ ...formData, nationality: newValue as string })}
+              isOptionEqualToValue={(option, value) => option === value}
             />
           </FormControl>
+
+          <Typography level="title-lg">Address</Typography>
+          <Divider />
+
+          <Checkbox
+            label="Landed property"
+            checked={formData.isLandedProperty}
+            onChange={(e) => setFormData({ ...formData, isLandedProperty: e.target.checked })}
+          />
+
+          {!formData.isLandedProperty && (
+            <Grid container columnSpacing={2}>
+              <Grid xs={6} style={{ paddingLeft: 0 }}>
+                <FormControl required>
+                  <FormLabel>Floor</FormLabel>
+                  <Input
+                    type="number"
+                    name="floor"
+                    value={formData.floor}
+                    onChange={handleInputChange}
+                  />
+                </FormControl>
+              </Grid>
+              <Grid xs={6} style={{ paddingRight: 0 }}>
+                <FormControl required>
+                  <FormLabel>Unit no.</FormLabel>
+                  <Input
+                    type="number"
+                    name="unit"
+                    value={formData.unit}
+                    onChange={handleInputChange}
+                  />
+                </FormControl>
+              </Grid>
+            </Grid>
+          )}
+
           <FormControl required>
             <FormLabel>Postal code</FormLabel>
             <Input
-              name="address.postalCode"
+              name="postalCode"
+              type="number"
+              value={formData.postalCode}
+              onChange={handlePostalCodeChange}
             />
+            {!formData.isPostalCodeValid && (
+              <Typography color="danger">Postal code is invalid</Typography>
+            )}
           </FormControl>
-          <Button onClick={handleBack}>Back</Button>
-          <Button onClick={handleSubmit}>Submit</Button>
 
-          <Typography>{JSON.stringify(userData)}</Typography>
+          {formData.postalCode.length === 6 && formData.isPostalCodeValid && (
+            <>
+              <FormControl required>
+                <FormLabel>Block</FormLabel>
+                <Input type="text" name="block" value={formData.addressDetails.block} disabled />
+              </FormControl>
+
+              <FormControl required>
+                <FormLabel>Street</FormLabel>
+                <Input type="text" name="street" value={formData.addressDetails.street} disabled />
+              </FormControl>
+
+              <FormControl required>
+                <FormLabel>Building</FormLabel>
+                <Input type="text" name="building" value={formData.addressDetails.building} disabled />
+              </FormControl>
+            </>
+          )}
+
+          <Button variant="outlined" onClick={handleBack}>Back</Button>
+          <Button onClick={handleSubmit} disabled={!isFormValid}>Submit</Button>
         </>
       ),
     },
