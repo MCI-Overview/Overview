0ab6d97c808684c76e7716b5b051c84544071612
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-06-26 15:20:27 +0800

    Fix lint error

--- attendance-module/src/components/project/overview/CandidateCount.tsx
+++ attendance-module/src/components/project/overview/CandidateCount.tsx
@@ -1,68 +1,66 @@
-import * as React from 'react';
-import AspectRatio from '@mui/joy/AspectRatio';
-import Box from '@mui/joy/Box';
-import Button from '@mui/joy/Button';
-import Card from '@mui/joy/Card';
-import CardContent from '@mui/joy/CardContent';
-import Typography from '@mui/joy/Typography';
-import Sheet from '@mui/joy/Sheet';
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Card from "@mui/joy/Card";
+import CardContent from "@mui/joy/CardContent";
+import Typography from "@mui/joy/Typography";
+import Sheet from "@mui/joy/Sheet";
 
 export default function CandidaeCount() {
-    return (
-        <Box
+  return (
+    <Box
+      sx={{
+        maxWidth: "400px",
+        position: "relative",
+        overflow: { xs: "auto", sm: "initial" },
+      }}
+    >
+      <Card
+        orientation="horizontal"
+        sx={{
+          width: "100%",
+        }}
+      >
+        <CardContent>
+          <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
+            Candidate count as of 12/34/56
+          </Typography>
+          <Sheet
             sx={{
-                maxWidth: "400px",
-                position: 'relative',
-                overflow: { xs: 'auto', sm: 'initial' },
+              bgcolor: "background.level1",
+              borderRadius: "sm",
+              p: 1.5,
+              my: 1.5,
+              display: "flex",
+              gap: 2,
+              "& > div": { flex: 1 },
             }}
-        >
-            <Card
-                orientation="horizontal"
-                sx={{
-                    width: '100%',
-                }}
-            >
-                <CardContent>
-                    <Typography level="body-sm" fontWeight="lg" textColor="text.tertiary">
-                        Candidate count as of 12/34/56
-                    </Typography>
-                    <Sheet
-                        sx={{
-                            bgcolor: 'background.level1',
-                            borderRadius: 'sm',
-                            p: 1.5,
-                            my: 1.5,
-                            display: 'flex',
-                            gap: 2,
-                            '& > div': { flex: 1 },
-                        }}
-                    >
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Active
-                            </Typography>
-                            <Typography fontWeight="lg">34</Typography>
-                        </div>
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Inactive
-                            </Typography>
-                            <Typography fontWeight="lg">980</Typography>
-                        </div>
-                        <div>
-                            <Typography level="body-xs" fontWeight="lg">
-                                Total
-                            </Typography>
-                            <Typography fontWeight="lg">8.9</Typography>
-                        </div>
-                    </Sheet>
-                    <Box sx={{ display: 'flex', gap: 1.5, '& > button': { flex: 1 } }}>
-                        <Button variant="solid" color="primary">
-                            View candidates
-                        </Button>
-                    </Box>
-                </CardContent>
-            </Card>
-        </Box>
-    );
+          >
+            <div>
+              <Typography level="body-xs" fontWeight="lg">
+                Active
+              </Typography>
+              <Typography fontWeight="lg">34</Typography>
+            </div>
+            <div>
+              <Typography level="body-xs" fontWeight="lg">
+                Inactive
+              </Typography>
+              <Typography fontWeight="lg">980</Typography>
+            </div>
+            <div>
+              <Typography level="body-xs" fontWeight="lg">
+                Total
+              </Typography>
+              <Typography fontWeight="lg">8.9</Typography>
+            </div>
+          </Sheet>
+          <Box sx={{ display: "flex", gap: 1.5, "& > button": { flex: 1 } }}>
+            <Button variant="solid" color="primary">
+              View candidates
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
