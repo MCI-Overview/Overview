d2eef37594da5549b9a72cbbe6fca770b7f361f2
ElginTZM <63971687+ElginTZM@users.noreply.github.com>
2024-07-16 11:25:43 +0800

    Fix sort option not visible on mobile

--- attendance-module/src/components/project/roster/WeekPicker.tsx
+++ attendance-module/src/components/project/roster/WeekPicker.tsx
@@ -9,12 +9,11 @@ import {
   Button,
   Select,
   Option,
-  IconButton,
+  Stack,
 } from "@mui/joy";
 import {
   KeyboardArrowRightRounded as KeyboardArrowRightIcon,
   KeyboardArrowLeftRounded as KeyboardArrowLeftIcon,
-  RefreshRounded as RefreshIcon,
 } from "@mui/icons-material";
 
 export default function WeekPicker() {
@@ -27,7 +26,6 @@ export default function WeekPicker() {
     setSortOrderBy,
     setWeekOffset,
     setDates,
-    updateRosterData,
   } = useRosterContext();
   const projectStartDate = dayjs(project?.startDate);
   const projectEndDate = dayjs(project?.endDate);
@@ -75,50 +73,53 @@ export default function WeekPicker() {
       </Button>
 
       <Box sx={{ flex: 1 }} />
-      <Button size="sm" variant="outlined" color="neutral">
-        {`${dateRangeStart?.format("DD/MM/YY")} -
+      <Stack
+        direction={{
+          xs: "column",
+          md: "row",
+        }}
+        justifyContent="center"
+        alignItems="center"
+        spacing={1}
+      >
+        <Button size="sm" variant="outlined" color="neutral">
+          {`${dateRangeStart?.format("DD/MM/YY")} -
         ${dateRangeEnd?.format("DD/MM/YY")}`}
-      </Button>
-      <Select
-        defaultValue="name-asc"
-        sx={{
-          width: "9rem",
-        }}
-        onChange={(_, value) => {
-          switch (value) {
-            case "name-asc":
-              setSortOrder("asc");
-              setSortOrderBy("name");
-              break;
-            case "name-desc":
-              setSortOrder("desc");
-              setSortOrderBy("name");
-              break;
-            case "unassign":
-              setSortOrder("desc");
-              setSortOrderBy("assign");
-              break;
-            case "assign":
-              setSortOrder("asc");
-              setSortOrderBy("assign");
-              break;
-            default:
-              break;
-          }
-        }}
-      >
-        <Option value="name-asc">Name (Asc)</Option>
-        <Option value="name-desc">Name (Desc)</Option>
-        <Option value="unassign">Unassigned</Option>
-        <Option value="assign">Assigned</Option>
-      </Select>
-      <IconButton
-        onClick={() => {
-          updateRosterData();
-        }}
-      >
-        <RefreshIcon />
-      </IconButton>
+        </Button>
+        <Select
+          defaultValue="name-asc"
+          sx={{
+            width: "9rem",
+          }}
+          onChange={(_, value) => {
+            switch (value) {
+              case "name-asc":
+                setSortOrder("asc");
+                setSortOrderBy("name");
+                break;
+              case "name-desc":
+                setSortOrder("desc");
+                setSortOrderBy("name");
+                break;
+              case "unassign":
+                setSortOrder("desc");
+                setSortOrderBy("assign");
+                break;
+              case "assign":
+                setSortOrder("asc");
+                setSortOrderBy("assign");
+                break;
+              default:
+                break;
+            }
+          }}
+        >
+          <Option value="name-asc">Name (Asc)</Option>
+          <Option value="name-desc">Name (Desc)</Option>
+          <Option value="unassign">Unassigned</Option>
+          <Option value="assign">Assigned</Option>
+        </Select>
+      </Stack>
       <Box sx={{ flex: 1 }} />
 
       <Button
